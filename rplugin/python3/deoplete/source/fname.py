1fdd03821dedcadeac50d3ecd3c6d097bcc8da25
hrsh7th <hrsh7th@gmail.com>
2019-04-19 19:10:48 +0900

    Update

--- rplugin/python3/deoplete/source/fname.py
+++ rplugin/python3/deoplete/source/fname.py
@@ -8,13 +8,17 @@ class Source(Base):
         self.name = 'fname'
         self.mark = '[fname]'
         self.rank = 500
-
-    def get_complete_position(self, context):
-        return len(context['input']) + 1
+        self.is_volatile = True
 
     def gather_candidates(self, context):
         if not exists(context['bufpath']):
             return []
         name = basename(context['bufpath'])
-        return [{'word': sub(r"\..*?$", '', name), 'menu': 'fn'}, {'word': name, 'menu': 'fn'}]
+        return [{
+            'word': name,
+            'dup': 1
+        }, {
+            'word': sub(r"\.[^\.]*$", '', name),
+            'dup': 1
+        }]
 
